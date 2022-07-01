40cc150255a5120aac7229018727808040e13e7d
yogendra131994 <87609565+yogendra131994@users.noreply.github.com>
2022-07-01 20:37:31 +0530

    Update dicejava.js

--- dicejava.js
+++ dicejava.js
@@ -1,12 +1,12 @@
 var random_num=Math.floor(Math.random()*6)+1;
 var image="dice"+random_num+".png";
-var final="dice-game/images/"+image;
+var final="images/"+image;
 document.querySelectorAll("img")[0].setAttribute("src",final);
 
 
 var random_num_2=Math.floor(Math.random()*6)+1;
 var image_2="dice"+random_num_2+".png";
-var final_2="dice-game/images/"+image_2;
+var final_2="images/"+image_2;
 document.querySelectorAll("img")[1].setAttribute("src",final_2);
 
 
